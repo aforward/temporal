0915ddf579c0803ce2d1a4d1b9670f4e643a4ce0
Andrew Forward <aforward@gmail.com>
2017-05-14 14:10:41 -0400

    v0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Temporal.Mixfile do
   @name    :temporal
   @friendly_name "Temporal"
   @description "A library to download documents from the interwebs."
-  @version "0.1.1"
+  @version "0.1.2"
   @git_url "https://github.com/aforward/temporal"
   @home_url @git_url
 
